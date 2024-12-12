1293c5727721815f56c972a002e0a682931e00b3
ppv-1 <virus9320@gmail.com>
2024-12-12 17:55:14 +0800

    Add paging

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,19 @@
 #![reexport_test_harness_main = "test_main"]
 #![feature(abi_x86_interrupt)]
 
+extern crate alloc;
+
 use core::panic::PanicInfo;
 
+#[cfg(test)]
+use bootloader::{entry_point, BootInfo};
+
+pub mod allocator;
 pub mod serial;
 pub mod vga_buffer;
 pub mod interrupts;
 pub mod gdt;
+pub mod memory;
 
 pub trait Testable {
     fn run(&self) -> ();
@@ -56,13 +63,15 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
     hlt_loop();
 }
 
+#[cfg(test)]
+entry_point!(test_kernel_main);
+
 /// Entry point for `cargo test`
 #[cfg(test)]
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+fn test_kernel_main(_boot_info: &'static BootInfo) -> ! {
     init();
     test_main();
-    hlt_loop()
+    hlt_loop();
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use bootloader::{BootInfo, entry_point};
+use x86_64::structures::paging::FrameAllocator;
 
 mod vga_buffer;
 mod serial;
@@ -73,13 +75,33 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
     }
 }
 
+entry_point!(kernel_main);
+
 // This is our entry point.
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use scrap_os::memory;
+    use scrap_os::memory::BootInfoFrameAllocator;
+    use x86_64::{structures::paging::Page, VirtAddr};
+
     println!("hello world{}", "!");
 
     scrap_os::init();
 
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe {
+        BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };
+
+    // map an unused page
+    let page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+    // write the string `New!` to the screen through the new mapping
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
 
     #[cfg(test)]
     test_main();
